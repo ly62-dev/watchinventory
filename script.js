108ed08f57c63cba891746b1cc86bcfbb162050a
ly62-dev <larayamamoto62@gmail.com>
2025-06-20 14:44:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -355,3 +355,17 @@ function loadDropdowns() {
     })
     .catch(error => console.error("Dropdown Fetch Error:", error));
 }
+
+function openLink(evt, linkName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("myLink");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
